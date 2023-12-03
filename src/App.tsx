cdfbdf5ba750bce165ce1d73f312a2b785305d8a
Hector Beita <hecbeita1988@gmail.com>
2023-12-02 18:56:54 -0600

    More scaffoling + storybook for component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './layouts/Sidebar/Sidebar';
+import DashboardPage from './pages/Dashboard/DashboardPage';
+import { Header } from './layouts';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Reports = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Reports</h1>
+    </div>
+  );
 }
 
-export default App
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/profile" element={<Profile />} />
+            {/* other routes */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import TestComponent from './components/TestComponent.tsx'
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <TestComponent />
   </React.StrictMode>,
 )
